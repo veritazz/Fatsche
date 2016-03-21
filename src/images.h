708c0203e1c8078aff20450612e19b9932d1dca9
Michael Gollnick <Michael.Gollnick@gmx.de>
2016-03-21 14:30:20 +0100

    remove unused frames from raider animation

--- src/images.h
+++ src/images.h
@@ -11,11 +11,11 @@ extern const uint8_t water_bomb_impact_img[34];
 extern const uint8_t player_img[30];
 extern const uint8_t player_all_frames_img[386];
 extern const uint8_t enemy1_all_frames_img[2050];
-extern const uint8_t enemy_raider_img[674];
+extern const uint8_t enemy_raider_img[450];
 extern const uint8_t enemy_grandma_img[226];
 extern const uint8_t scene_lamp_img[44];
 extern const uint8_t numbers_3x5_img[32];
 
-/* total size 5598 bytes */
+/* total size 5374 bytes */
 
 #endif
